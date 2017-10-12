8c204c03684fdf98fb7b4a0c898840a5144816d6
mhcvs2 <merzhong0501@163.com>
2017-10-12 23:12:51 +0000

    add

--- config.py
+++ config.py
@@ -3,29 +3,23 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    def __init__(self):
-        pass
-
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '!@#$%^&*12345678'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    # If use QQ email, please see http://service.mail.qq.com/cgi-bin/help?id=28 firstly.
     MAIL_SERVER = 'smtp.qq.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FORUM_MAIL_SUBJECT_PREFIX = 'NaHan'
-    FORUM_MAIL_SENDER = 'Nahan <selfboot@qq.com>'
-
-    BABEL_DEFAULT_LOCALE = 'zh'
-    BABEL_DEFAULT_TIMEZONE = 'CST'
-
-    PER_PAGE = 10
-    UPLOAD_FOLDER = os.path.join(basedir, 'nahan/static/upload')
-    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-    MAX_CONTENT_LENGTH = 512 * 1024
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '591867837@qq.com'
+    MAIL_PASSWORD = 'xxibvizgihlmbcgb'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[mhc]'
+    FLASKY_MAIL_SENDER = 'Mhc Admin <591867837@qq.com>'
+    FLASKY_ADMIN = '591867837@qq.com'
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
+    FLASKY_SLOW_DB_QUERY_TIME=0.5
 
     @staticmethod
     def init_app(app):
@@ -33,23 +27,85 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    def __init__(self):
-        pass
-
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = (os.environ.get('DEV_DATABASE_URL') or
-                               'mysql://root:root.123@localhost/nahan')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mhc.123@127.0.0.1/test1'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
-    def __init__(self):
-        pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
-    SQLALCHEMY_DATABASE_URI = (os.environ.get('DEV_DATABASE_URL') or
-                               'mysql://root:123456@localhost/nahan')
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+class HerokuConfig(ProductionConfig):
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # handle proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
 
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
+    'unix': UnixConfig,
+
     'default': DevelopmentConfig
 }
